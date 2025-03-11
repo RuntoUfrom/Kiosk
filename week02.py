fd7daf3bb611a7078ecb2a5644d5df8033900010
uyeun <jjakklv@naver.com>
2025-03-11 20:25:11 +0900

    add total price

--- week02.py
+++ week02.py
@@ -1,14 +1,18 @@
 drinks = ["Ice Americano","Cafe Latte"]
 prices = [2000,3000]
-
+total_price = 0
 while True:
     menu = input(f"1) {drinks[0]} {prices[0]}won 2) {drinks[1]} {prices[1]}won  3) Exit")
     if menu =="1":
         print(f"{drinks[0]}  ordered. price is {prices[0]}")
+        total_price+=prices[0]
     elif menu == "2":
         print(f"{drinks[1]} ordered. price is {prices[1]}")
+        total_price+=prices[1]
     elif menu =="3":
         print("finish order")
         break
     else:
         print(f"{menu} menu is not exist. ")
+
+print(f"total price is {total_price}")
