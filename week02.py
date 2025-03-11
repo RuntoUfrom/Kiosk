dc710a79d60a16a1fdfee3602930058891afd4c6
uyeun <jjakklv@naver.com>
2025-03-11 19:57:11 +0900

    menu add

---
+++ week02.py
@@ -0,0 +1,7 @@
+menu = input("1) Ice Americano  2) Cafe Latte  3) Exit")
+if menu =="1":
+    print("Ice Americano ordered. price is 2000")
+elif menu == "2":
+    print("Cafe Latte ordered. price is 3000")
+elif menu =="3":
+    print("finish order")
